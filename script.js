8dd054083e165697b24a9ce3dee68cd933fcf9e8
Afzaal-Web <afzaalwebsolutions@gmail.com>
2025-06-11 20:34:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ const currentDateParagraph = document.getElementById('current-date');
 const dateOptionsSelectElement = document.getElementById('date-options');
 
 
-
+let timer = 0;
 
 // const exampleSentence = "laazfa".split("").reverse().join();
 // console.log(exampleSentence);
 
 dateOptionsSelectElement.addEventListener('change', () => {
+    clearInterval(timer);
+
     const date = new Date();
     const day =  date.getDate();
     const month = date.getMonth() + 1;
     const year = date.getFullYear();
     const hours = date.getHours();
-    const minutes = date.getMinutes();
-    const seconds = date.getSeconds();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
     
     const formattedDate = `${day}-${month}-${year}`;
 
@@ -25,18 +27,28 @@ switch(dateOptionsSelectElement.value){
     break;
 
     case 'mm-dd-yyyy-h-mm':
-    const formattedHours = hours % 12 || 12;
+    const formattedHours = (hours % 12 || 12).toString().padStart(2, '0');
     const timePeriod = hours >= 12 ? 'PM' : 'AM';
-    const paddedMinutes = minutes < 10 ? `0${minutes}` : minutes;
-    const paddedseconds = seconds < 10 ? `0${seconds}` : seconds;
-    currentDateParagraph.textContent = `${month}-${day}-${year} ${formattedHours}:${paddedMinutes} ${paddedseconds} ${timePeriod}`;
+    currentDateParagraph.textContent = `${month}-${day}-${year} ${formattedHours}:${minutes} ${seconds} ${timePeriod}`;
     confetti({});
+    timer = setInterval(() => {
+        const liveDate = new Date();
+        const liveHours = (liveDate.getHours() % 12 || 12).toString().padStart(2, '0');
+        const liveMinutes = liveDate.getMinutes().toString().padStart(2, '0');
+        const liveSeconds = liveDate.getSeconds().toString().padStart(2, '0');
+        const timePeriod = liveDate.getHours() >= 12 ? 'PM' : 'AM';
+        currentDateParagraph.textContent = `${month}-${day}-${year} ${liveHours}:${liveMinutes}:${liveSeconds} ${timePeriod}`;
+      }, 1000);
     break;
     
     default:
         currentDateParagraph.textContent = formattedDate;
 }
+
+
+
 });
+
 
 
 
